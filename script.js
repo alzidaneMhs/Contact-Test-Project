9b4ef7e2205612f6abf29d3ee8fab7baffdbb659
azenenen <alzidane2006@gmail.com>
2024-12-06 02:23:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var modal = document.querySelector(".modal-container");
+var button = document.querySelector("button");
+var btnClose = document.querySelector("span");
+if (button) {
+    button.addEventListener("click", function() {
+      modal.style.visibility = "visible";
+    });
+  }
+  if (btnClose) {
+    btnClose.addEventListener("click", function() {
+       modal.style.visibility = "hidden";
+    });
+  }
